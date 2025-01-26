6828d01e499d4a9f8c15e5cf6ff6c227cabddd93
Josemaria Ofurum <Josemariaofurum@gmail.com>
2025-01-26 13:37:23 +0100

    implement setup for project with tailwind

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+import logo from "../assets/icons/logo.svg";
+import line from "../assets/icons/line.svg";
+import comb from "../assets/icons/comb.svg";
+import clipper from "../assets/icons/clipper.svg";
+
+const navlinks = [
+  { title: "Home" },
+  { title: "About Us" },
+  { title: "Services" },
+  { title: "Gallery" },
+  { title: "Testimonials" },
+];
+
+const heroTitle = "Look Good. Feel Great. Every Time";
+
+const icons = { logo, line, comb, clipper };
+
+const images = {};
+
+export { icons, images, navlinks, heroTitle };
